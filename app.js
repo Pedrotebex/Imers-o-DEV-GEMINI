5be94b8c267a26a973db3311b6586b4ff1dd3932
Pedro Neto <pedrotebex@gmail.com>
2024-09-06 09:43:12 -0300

    Adicionando Animações ao projeto

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+// Função de busca
 document.querySelector('button').addEventListener('click', function() {
     // Captura o valor do input de pesquisa
     const searchTerm = document.querySelector('input[type="text"]').value.toLowerCase();
 
-    // Seleciona todos os cards de atletas
+    // Seleciona todos os cards de personagens
     const cards = document.querySelectorAll('.card-personagem');
 
-    // Itera sobre os cards e verifica se o texto do nome do atleta corresponde ao termo de pesquisa
+    // Itera sobre os cards e verifica se o texto do nome corresponde ao termo de pesquisa
     cards.forEach(card => {
         const NomePersonagem = card.querySelector('h2').textContent.toLowerCase();
         
-        // Se o nome do atleta incluir o termo de pesquisa, mostra o card, senão, oculta
+        // Se o nome do personagem incluir o termo de pesquisa, mostra o card, senão, oculta
         if (NomePersonagem.includes(searchTerm)) {
             card.style.display = 'block';
         } else {
